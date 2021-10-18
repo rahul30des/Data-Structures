77ee1d6b88d637716bb3a270518d1e9cd783725e
Rahul Deshpande <rahul30des@gmail.com>
2021-10-18 06:52:28 -0700

    Min number of stairs

---
+++ stairs/stairs.cpp
@@ -0,0 +1,20 @@
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n+1, -1);
+        return climbStairsUtil(n, dp);
+    }
+    int climbStairsUtil(int n, vector<int>& dp) {
+        if(dp[n] == -1) {
+            if(n == 0) {
+                dp[n] = 0;
+            } else if (n == 1) {
+                dp[1] = 1;    
+            } else if (n == 2) {
+                dp[2] = 2;
+            } else {
+                dp[n] = climbStairsUtil(n-2, dp) + climbStairsUtil(n-1, dp);
+            }
+        }
+        return dp[n];
+    }
+};
